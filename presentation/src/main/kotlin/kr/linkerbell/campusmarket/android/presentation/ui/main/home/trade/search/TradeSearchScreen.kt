fb8bd1caaaffa4550a8cc51872299d64cb46145c
Ray Jang <ajou4095@gmail.com>
2024-11-15 21:36:51 +0900

    search

--- presentation/src/main/kotlin/kr/linkerbell/campusmarket/android/presentation/ui/main/home/trade/search/TradeSearchScreen.kt
+++ presentation/src/main/kotlin/kr/linkerbell/campusmarket/android/presentation/ui/main/home/trade/search/TradeSearchScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,32 +12,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.plus
 import kr.linkerbell.campusmarket.android.common.util.coroutine.event.MutableEventFlow
 import kr.linkerbell.campusmarket.android.presentation.R
-import kr.linkerbell.campusmarket.android.presentation.common.theme.Indigo50
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Gray900
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Headline3
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Space20
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Space24
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Space56
+import kr.linkerbell.campusmarket.android.presentation.common.theme.Space8
+import kr.linkerbell.campusmarket.android.presentation.common.theme.White
 import kr.linkerbell.campusmarket.android.presentation.common.util.compose.makeRoute
+import kr.linkerbell.campusmarket.android.presentation.common.util.compose.safeNavigateUp
+import kr.linkerbell.campusmarket.android.presentation.common.view.RippleBox
+import kr.linkerbell.campusmarket.android.presentation.common.view.confirm.ConfirmButton
+import kr.linkerbell.campusmarket.android.presentation.common.view.confirm.ConfirmButtonProperties
+import kr.linkerbell.campusmarket.android.presentation.common.view.confirm.ConfirmButtonSize
+import kr.linkerbell.campusmarket.android.presentation.common.view.confirm.ConfirmButtonType
 import kr.linkerbell.campusmarket.android.presentation.common.view.textfield.TypingTextField
 import kr.linkerbell.campusmarket.android.presentation.ui.main.home.trade.TradeConstant
 import kr.linkerbell.campusmarket.android.presentation.ui.main.home.trade.search.result.TradeSearchResultConstant
@@ -49,6 +65,9 @@ fun TradeSearchScreen(
     argument: TradeSearchArgument,
     data: TradeSearchData
 ) {
+    val (state, event, intent, logEvent, coroutineContext) = argument
+    val scope = rememberCoroutineScope() + coroutineContext
+
     var queryString by remember { mutableStateOf(data.previousQuery) }
 
     val navigateToResultScreen = { queryName: String ->
@@ -66,10 +85,11 @@ fun TradeSearchScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Indigo50)
+            .background(White)
     ) {
         TradeSearchScreenSearchBar(
-            queryString,
+            navController = navController,
+            currentQuery = queryString,
             onValueChange = { updatedQuery ->
                 queryString = updatedQuery
             },
@@ -82,7 +102,6 @@ fun TradeSearchScreen(
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
-
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -90,15 +109,27 @@ fun TradeSearchScreen(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text("최근 검색 내역")
-                Text(text = "전체 삭제",
-                    modifier = Modifier.clickable {
+                Text(
+                    text = "최근 검색 내역",
+                    style = Headline3.merge(Gray900)
+                )
+                ConfirmButton(
+                    properties = ConfirmButtonProperties(
+                        size = ConfirmButtonSize.Small,
+                        type = ConfirmButtonType.Secondary
+                    ),
+                    onClick = {
                         argument.intent(TradeSearchIntent.DeleteAll)
                     }
-                )
+                ) { style ->
+                    Text(
+                        text = "전체 삭제",
+                        style = style
+                    )
+                }
             }
 
-            data.searchHistory.forEach { searchHistory ->
+            data.searchHistory.reversed().forEach { searchHistory ->
                 TradeSearchScreenSearchHistoryCard(
                     searchHistory,
                     onClearIconClick = {
@@ -112,72 +143,90 @@ fun TradeSearchScreen(
             }
         }
     }
-
 }
 
 @Composable
 private fun TradeSearchScreenSearchBar(
+    navController: NavController,
     currentQuery: String,
     onValueChange: (String) -> Unit,
     onSearchIconClick: (String) -> Unit
 ) {
+    val focusRequester = remember { FocusRequester() }
 
-    Box(
+    Row(
         modifier = Modifier
-            .background(Color.White)
+            .height(Space56)
+            .background(White)
             .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        )
-        {
-            Row(
-                modifier = Modifier
-                    .weight(8f)
-                    .height(36.dp)
-                    .clip(RoundedCornerShape(5.dp))
-                    .background(Indigo50),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                TypingTextField(
-                    text = currentQuery,
-                    onValueChange = { onValueChange(it) },
-                    hintText = "검색어를 입력하세요",
-                    maxLines = 1,
-                    maxTextLength = 100,
-                    trailingIconContent = {
-                        Icon(
-                            imageVector = Icons.Default.Clear,
-                            contentDescription = "Clear button",
-                            modifier = Modifier
-                                .padding(end = 8.dp)
-                                .size(20.dp)
-                                .clickable {
-                                    onValueChange("")
-                                }
-                        )
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                )
+        RippleBox(
+            modifier = Modifier.padding(start = Space20),
+            onClick = {
+                navController.safeNavigateUp()
             }
-            Spacer(Modifier.padding(4.dp))
+        ) {
             Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search Button",
-                modifier = Modifier
-                    .size(24.dp)
-                    .weight(1f)
-                    .clickable {
-                        if (currentQuery.isNotBlank())
-                            onSearchIconClick(currentQuery)
-                    }
+                modifier = Modifier.size(Space24),
+                painter = painterResource(R.drawable.ic_chevron_left),
+                contentDescription = null,
+                tint = Gray900
             )
         }
+        TypingTextField(
+            text = currentQuery,
+            onValueChange = { onValueChange(it) },
+            hintText = "검색어를 입력하세요",
+            maxLines = 1,
+            maxTextLength = 100,
+            trailingIconContent = {
+                if (currentQuery.isNotEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear button",
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clickable {
+                                onValueChange("")
+                            }
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    if (currentQuery.isNotBlank()) {
+                        onSearchIconClick(currentQuery)
+                    }
+                }
+            ),
+            modifier = Modifier
+                .padding(horizontal = Space20)
+                .weight(1f)
+                .focusRequester(focusRequester)
+        )
+
+        RippleBox(
+            modifier = Modifier.padding(end = Space20),
+            onClick = {
+                if (currentQuery.isNotBlank()) {
+                    onSearchIconClick(currentQuery)
+                }
+            }
+        ) {
+            Icon(
+                modifier = Modifier.size(Space24),
+                painter = painterResource(id = R.drawable.ic_search),
+                contentDescription = null,
+                tint = Gray900
+            )
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 }
 
@@ -187,48 +236,56 @@ private fun TradeSearchScreenSearchHistoryCard(
     onClearIconClick: () -> Unit,
     onTextClick: (String) -> Unit
 ) {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+            .clickable {
+                onTextClick(history)
+            }
     ) {
+        Spacer(modifier = Modifier.height(Space8))
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_clock),
-                contentDescription = "Clock Image",
-                modifier = Modifier
-                    .padding(end = 8.dp)
-                    .size(12.dp)
-            )
-            Text(
-                text = history,
-                fontSize = 12.sp,
-                modifier = Modifier.clickable {
-                    onTextClick(history)
-                }
-            )
-        }
-        Icon(
-            imageVector = Icons.Default.Clear,
-            contentDescription = "Clear button",
-            modifier = Modifier
-                .padding(end = 8.dp)
-                .size(20.dp)
-                .clickable {
+            Spacer(modifier = Modifier.width(Space8))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_clock),
+                    contentDescription = "Clock Image",
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                        .size(12.dp)
+                )
+                Text(
+                    text = history,
+                    fontSize = 12.sp
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            RippleBox(
+                onClick = {
                     onClearIconClick()
                 }
-        )
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(Space24),
+                    painter = painterResource(R.drawable.ic_close),
+                    contentDescription = null,
+                    tint = Gray900
+                )
+            }
+            Spacer(modifier = Modifier.width(Space8))
+        }
+        Spacer(modifier = Modifier.height(Space8))
     }
 }
 
 @Preview
 @Composable
 private fun TradeSearchScreenPreview() {
-
     TradeSearchScreen(
         navController = rememberNavController(),
         argument = TradeSearchArgument(
