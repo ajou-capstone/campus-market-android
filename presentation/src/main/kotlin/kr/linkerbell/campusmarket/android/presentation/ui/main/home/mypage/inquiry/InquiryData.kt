670e357cb07958df04f395ece6e43992f10c4261
Sirius_Alpha <scgv2222@naver.com>
2024-11-25 01:31:45 +0900

    [CM-35.1] 다른 사용자 프로필 확인 UI & 로직 제작
    - delete

--- presentation/src/main/kotlin/kr/linkerbell/campusmarket/android/presentation/ui/main/home/mypage/inquiry/InquiryData.kt
+++
@@ -1,8 +0,0 @@
-package kr.linkerbell.campusmarket.android.presentation.ui.main.home.mypage.inquiry
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-data class InquiryData(
-    val initialData: String
-)
