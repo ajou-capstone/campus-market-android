06e489b80a62e72bdf266e1fa0fb6f6189ea1d2e
Sirius_22_lt <scgv2222@naver.com>
2024-11-25 13:26:07 +0900

    [35.1] 다른 사용자 프로필 확인 UI & 로직 제작
    - API param

--- data/src/main/kotlin/kr/linkerbell/campusmarket/android/data/remote/network/api/feature/MyPageApi.kt
+++ data/src/main/kotlin/kr/linkerbell/campusmarket/android/data/remote/network/api/feature/MyPageApi.kt
@@ -34,11 +34,12 @@ class MyPageApi @Inject constructor(
         userId: Long,
         page: Int,
         size: Int,
+        type: String
     ): Result<RecentTradeRes> {
         return client.get("$baseUrl/api/v1/items/$userId/history") {
             parameter("page", page.toString())
             parameter("size", size.toString())
-            parameter("type", type.toString())
+            parameter("type", type)
         }.convert(errorMessageMapper::map)
     }
 }
